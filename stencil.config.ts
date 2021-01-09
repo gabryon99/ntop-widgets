75c61d5a575230049a7937bc1e1c9295db98050d
gabryon99 <pappalardo@ntop.org>
2021-01-09 15:58:02 +0100

    updated global dependecies

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,7 @@
 import { Config } from '@stencil/core';
 
+// https://stackoverflow.com/questions/60633526/how-to-use-an-external-third-party-library-in-stencil-js
+
 export const config: Config = {
   namespace: 'ntop-widgets',
   outputTargets: [
@@ -15,6 +17,16 @@ export const config: Config = {
     },
     {
       type: 'www',
+      copy: [
+        {
+          src: 'js/d3.v3.js',
+          dest: 'lib/d3.v3.js'
+        },
+        {
+          src: 'js/nv.d3.js',
+          dest: 'lib/nv.d3.js'
+        }
+      ],
       serviceWorker: null, // disable service workers
     },
   ],
